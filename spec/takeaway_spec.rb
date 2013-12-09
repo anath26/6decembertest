69c1874262bbaeb2c048a4d4f0bd4fc7d4d9bb1c
Evgeny Shadchnev <evgeny.shadchnev@gmail.com>
2013-12-09 16:29:07 +0000

    Feedback

--- spec/takeaway_spec.rb
+++ spec/takeaway_spec.rb
@@ -5,23 +5,25 @@ describe Takeaway do
 	context 'menu' do
 		let(:takeaway) {Takeaway.new}
 
-		it ' should be able to list the dishes with there prices' do
+		it 'should be able to list the dishes with there prices' do
+			# this test doesn't test what it promises on the previous line
+			# instead, it could expect something along the lines of 
+			#    expect(Takeaway::MENU.keys).to include(:pizza)
+			#    expect(Takeaway::MENU.keys).to include(:ice_cream)
+			# You don't have to have a special menu() method
 			expect(takeaway.menu).to be_true
 		end
 
 		it 'should list 3 takeaway dishes' do
+			# Yep, this makes sense
 			expect(takeaway.menu.length).to eq(3)
 		end
 
 		it 'should allow ordering 2 pizza and 1 drink' do
+			# this test makes sense as well
 			order = [{:pizza => 2}, {:drink => 1}]
 			expect{takeaway.place_order(order, 19)}.to raise_error
 		end
-
-
-			
-
-
 		
 	end
 end
